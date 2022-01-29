a0477d097e513500f89005a3fd1c908dee3a5f57
Lukas Kopecky <56082570+kopeckylukas@users.noreply.github.com>
2022-01-29 21:44:05 +0000

    Upload of Beds Occupnacy
    Beds Occupancy by regions

---
+++ R/Beds_coversion.R
@@ -0,0 +1,17 @@
+
+library(tidyverse)
+library(readxl)
+
+
+### File: Beds 1
+
+b1 <- read_excel("Data/Beds/Beds.xlsx", sheet=1)
+b2 <- read_excel("Data/Beds/Beds.xlsx", sheet=2)
+b3 <- read_excel("Data/Beds/Beds.xlsx", sheet=3) 
+
+beds <- rbind(b1, b2)
+beds <- rbind(beds, b3)
+
+beds$South_of_England <- beds$`South East` + beds$`South West`
+beds$North_of_England <- beds$`North East and Yorkshire` + beds$`North West`
+beds$Midlands_and_East_of_England <- beds$Midlands + beds$`East of England`
